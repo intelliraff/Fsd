17b0a3eec003907c4879adab47c086a3f4d677cb
HelloBro8690 <hellobro8690@gmail.com>
2026-03-05 12:13:20 +0530

    5 March 2026

--- rat-app/src/app/app.ts
+++ rat-app/src/app/app.ts
@@ -18,6 +18,10 @@ export class App {
 }
   */
 
+
+/*
+lab 7-1:
+
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
@@ -34,6 +38,49 @@ export class App {
     console.log(form.value);
     console.log(form.valid);
 
+  }
+
+}
+  */
+
+import {Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector:'app-root',
+  standalone:false,
+  templateUrl:'./lab7_2.html'
+
+})
+
+export class App{
+  a: number = 0;
+  b: number = 0;
+  result: number | null = null;
+  Add() {
+    this.result = this.a + this.b;
+  }
+  
+  c: number=0;
+  d: number=0;
+  result2 :number |null=null;
+  Sub(){
+    this.result2=this.c-this.d;
+  }
+
+  e: number=0;
+  f: number=0;
+  result3: number|null=null;
+  Mul(){
+    this.result3=this.e*this.f;
+  }
+
+  g: number=0;
+  h: number=0;
+  result4: number|null=null;
+  Div(){
+    this.result4=this.g/this.f;
   }
 
 }
